8ddef07be4af9f89fda1753c5ebd356bdedb61da
sizzziy <sizzziy@yandex.ru>
2019-01-20 22:17:18 +0300

    created simple main page

--- votingisover/votingisover/urls.py
+++ votingisover/votingisover/urls.py
@@ -1,4 +1,4 @@
-"""votingisover URL Configuration
+"""landing URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
@@ -19,5 +19,5 @@ from voting import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index)
+    path('', views.index),
 ]
