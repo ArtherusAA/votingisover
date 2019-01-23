2ee101d0e904309c27b55e6d28330ba2b3a625d2
EgorovAlexey <EgorovAlexey@god.sky>
2019-01-23 20:18:00 +0300

    models added

--- votingisover/voting/models.py
+++ votingisover/voting/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class VotingInformation(models.Model):
+    type = models.CharField(max_length=100)
+    variantFirstCnt = models.IntegerField()
+    variantSecondCnt = models.IntegerField()
+
+class VotingDescription(models.Model):
+    header = models.CharField(max_length=1024)
+    variantFirstDescr = models.CharField(max_length=1000)
+    variantSecondDescr = models.CharField(max_length=1000)
