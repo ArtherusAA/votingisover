045aba31b00c78b70ddb3098bda29eac8be6c43c
Artherus <aarchy333@mail.ru>
2019-01-24 13:35:17 +0300

    pictures added

--- votingisover/votingisover/urls.py
+++ votingisover/votingisover/urls.py
@@ -17,6 +17,10 @@ from django.contrib import admin
 from django.urls import path
 from voting import views
 from django.contrib.auth import views as auth_views
+##from . import views, settings
+from django.contrib.staticfiles.urls import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,3 +32,6 @@ urlpatterns = [
     path('exit/', views.exit),
     path('voting/', views.voting)
 ]
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
