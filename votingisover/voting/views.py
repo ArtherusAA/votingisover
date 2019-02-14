005a1c702e54b9b56a313285d2f0b081fff2b8aa
EgorovAlexey <EgorovAlexey@god.sky>
2019-02-14 21:52:25 +0300

    Some bugs fixed

--- votingisover/voting/views.py
+++ votingisover/voting/views.py
@@ -45,23 +45,41 @@ def voting(request):
         "id": voting.id, "variants": variants})
         ##context['votings'].append(VotingForm(header = voting.header,
                                 ##type = voting.type, vote = variants))
+    if request.method == 'POST':
+        pass
     return render(request, 'registration/votingisover.html', context)
 
 def make_voting(request):
     context = {}
     current_user = request.user
     context['username'] = current_user
+    context['form'] = CreateVotingForm()
+    context['form'].variants = []
     if request.method == 'POST':
         keys = request.POST.keys()
-        voting = Voting(type = request.POST['type'], header = request.POST['header'])
-        voting.save()
-        variants = []
+        checkType = checkHeader = checkVariant1 = checkVariant2 = False
         for k in keys:
-            if k != 'type' and k != 'header' and k[:7] == 'Variant':
-                variants.append(Variant(text = request.POST[k], voting_id = voting))
-                variants[-1].save()
+            if k == 'header':
+                checkHeader = True
+            elif k == 'type':
+                checkType = True
+            elif k == 'Variant 1' and request.POST[k] != "":
+                checkVariant1 = True
+            elif k == 'Variant 2' and request.POST[k] != "":
+                checkVariant2 = True
+
+        if checkType and checkHeader and checkVariant1 and checkVariant2:
+            voting = Voting(type = request.POST['type'], header = request.POST['header'])
+            voting.save()
+            variants = []
+            for k in keys:
+                if k != 'type' and k != 'header' and k[:7] == 'Variant':
+                    variants.append(Variant(text = request.POST[k], voting_id = voting))
+                    variants[-1].save()
+        else:
+            context['form'] = CreateVotingForm()
     else:
-        context['form'] = CreateVotingForm()
+        #context['form'] = CreateVotingForm()
         context['form'].variants.append(forms.CharField(label="Variant 1"))
         context['form'].variants.append(forms.CharField(label="Variant 2"))
     return render(request, 'make_voting.html', context)
